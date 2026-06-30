0d90b07a9647255cd87c0277b5ba19cb0eb9fe49
Daniel Morales <lynxcapu1221@gmail.com>
2026-06-29 18:08:13 -0600

    Limpiando warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.lockdapp"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.lockdapp"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
